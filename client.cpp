4cb37f61ee042f0eb6987ce418ab5d11b0e3b2aa
kanishkdhebana <dhebanakanishk@gmail.com>
2025-06-14 18:52:59 +0530

    .

--- client.cpp
+++ client.cpp
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -7,6 +8,108 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+
+const size_t kMaxMessage = 4096 ;
+
+int32_t writeFull(int clientFd, const char* buffer, size_t size) {
+    while (size > 0) {
+        ssize_t bytesWritten = write(clientFd, buffer, size) ;
+        
+        if (bytesWritten <= 0) {
+            return -1 ; 
+        }   
+        
+        assert((size_t)bytesWritten <= size) ;
+
+        size -= (size_t)bytesWritten ;
+        buffer += bytesWritten ;
+    }
+
+    return 0 ;
+}
+
+
+int32_t readFull(int clientFd, char* buffer, size_t size) {
+    while (size > 0) {
+        int bytesRead = read(clientFd, buffer, size) ;
+        
+        if (bytesRead <= 0) { 
+            return -1 ; 
+        } 
+
+        assert((size_t)bytesRead <= size) ;
+
+        size -= (size_t)bytesRead ;
+        buffer += bytesRead ;
+    }
+    
+    return 0 ;
+}
+
+
+int32_t query(int fd, const char* message) {
+    uint32_t messageLength = (uint32_t)strlen(message) ;
+
+    if (messageLength > kMaxMessage) {
+        fprintf(stderr, "Message too long: %d bytes\n", messageLength) ;
+        return -1 ;
+    }
+    
+    // ___________later -> try dynamic allocation
+    char wbuf[4 + kMaxMessage] ;
+    memcpy(wbuf, &messageLength, 4) ; 
+    memcpy(wbuf + 4, message, messageLength) ;
+
+    int err = writeFull(fd, wbuf, 4 + messageLength) ;
+
+    if (err) {
+        perror("writeFull") ;
+        return -1 ;
+    }
+
+    else {
+        printf("Client(size = %d): %.*s\n", 4 + messageLength, (int)messageLength, message) ;
+    }
+
+    char rbuf[4 + kMaxMessage] ;
+    err = readFull(fd, rbuf, 4) ;
+    
+    if (err) {
+        const char* msg = "EOF" ;
+
+        if (errno != 0) {
+            msg = "read() error" ;
+        }
+
+        fprintf(stderr, "%s\n", msg) ;
+        return err ;
+    }
+
+    int responseLength = 0 ;
+    memcpy(&responseLength, rbuf, 4) ; 
+    
+    if (responseLength > kMaxMessage) {
+        fprintf(stderr, "Response too long: %d bytes\n", responseLength) ;
+        return -1 ;
+    }
+
+    err = readFull(fd, rbuf + 4, responseLength) ;
+    
+    if (err) {
+        const char* msg = "EOF" ;
+
+        if (errno != 0) {
+            msg = "read() error" ;
+        }
+
+        fprintf(stderr, "%s\n", msg) ;
+        return err ;
+    }
+
+    printf("Server(size = %d): %.*s\n", responseLength, responseLength, rbuf + 4) ;
+    return 0 ;
+}
+
 
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0) ;
@@ -37,34 +140,26 @@ int main() {
         printf("Connected to server successfully\n") ;
     }
 
-    
-    const char* message = "Hello, Server!" ;
-    ssize_t bytesWritten = write(fd, message, strlen(message)) ;
-    
-    if (bytesWritten < 0) {
-        perror("write") ;
-        close(fd) ;
-        exit(EXIT_FAILURE) ;    
-    }
-    
-    else {
-        printf("Wrote %zd bytes to the socket\n", bytesWritten) ;
+
+    int32_t err = query(fd, "kanishk");
+    if (err) {
+        printf("Error in query 1: %d\n", err);
+        goto L_DONE;
     }
 
-    char readBuffer[1024] = {} ;
-    ssize_t bytesRead = read(fd, readBuffer, sizeof(readBuffer) - 1) ;
-    
-    if (bytesRead < 0) {        
-        perror("read") ;
-        close(fd) ;
-        exit(EXIT_FAILURE) ;
-    } 
-    
-    else {
-        readBuffer[bytesRead] = '\0' ;
-        printf("Read %zd bytes: %s\n", bytesRead, readBuffer) ;
+    err = query(fd, "hello2");
+
+    if (err) {
+        goto L_DONE;
     }
 
-    close(fd) ;
-    return 0 ;
+    err = query(fd, "hello3");
+    
+    if (err) {
+        goto L_DONE;
+    }
+
+L_DONE:
+    close(fd);
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -8,16 +9,121 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 
-int main() {
-    int fd = socket(AF_INET, SOCK_STREAM, 0) ;
+const size_t kMaxMessage = 4096 ;
+
+int32_t writeFull(int clientFd, const char* buffer, size_t size) {
+    while (size > 0) {
+        ssize_t bytesWritten = write(clientFd, buffer, size) ;
+        
+        if (bytesWritten <= 0) {
+            return -1 ; 
+        }   
+        
+        assert((size_t)bytesWritten <= size) ;
+
+        size -= (size_t)bytesWritten ;
+        buffer += bytesWritten ;
+    }
+
+    return 0 ;
+}
+
+
+int32_t readFull(int clientFd, char* buffer, size_t size) {
+    while (size > 0) {
+        int bytesRead = read(clientFd, buffer, size) ;
+
+        if (bytesRead <= 0) { 
+            return -1 ; 
+        } 
+
+        assert((size_t)bytesRead <= size) ;
+
+        size -= (size_t)bytesRead ;
+        buffer += bytesRead ;
+    }
     
-    if (fd < 0) {
+    return 0 ;
+}
+
+int singleClientComm(int clientFd) {
+    char rbuf[4 + kMaxMessage] ;
+    errno = 0 ;
+
+    int err = readFull(clientFd, rbuf, 4) ;
+    
+    if (err) {
+        const char* msg = "EOF" ;
+
+        if (errno != 0) {
+            msg = "read() error" ;
+        }
+
+        return err ;
+    }
+
+    int clientMessageLength = 0 ;
+    memcpy(&clientMessageLength, rbuf, 4) ; 
+    
+    if (clientMessageLength > kMaxMessage) {
+        fprintf(stderr, "Message too long: %d bytes\n", clientMessageLength) ;
+        return -1 ;
+    }
+
+    err = readFull(clientFd, rbuf + 4, clientMessageLength) ;
+
+    if (err) {
+        const char* msg = "EOF" ;
+
+        if (errno != 0) {
+            msg = "read() error" ;
+        }
+
+        return err ;
+    }
+
+    printf("Client(%d): %.*s\n", clientMessageLength, clientMessageLength, rbuf + 4) ;
+
+    // reply
+    const char reply[] = "Hey buddy, I got your message!" ;
+    
+    if (strlen(reply) > kMaxMessage) {
+        fprintf(stderr, "Reply too long: %zu bytes\n", strlen(reply)) ;
+        return -1 ;
+    }
+
+
+    char wbuf[4 + sizeof(reply)] ;
+    int replyLength = strlen(reply) ;
+
+    memcpy(wbuf, &replyLength, 4) ;
+    memcpy(wbuf + 4, reply, replyLength) ; 
+    
+    err = writeFull(clientFd, wbuf, 4 + replyLength) ;
+    
+    if (err) {
+        perror("writeFull") ;
+        return -1 ;
+    }
+    
+    else {
+        printf("Server(%d): %.*s\n", 4 + replyLength, (int)replyLength, reply) ;
+    }
+
+    return 0 ;
+}
+
+
+int main() {
+    int serverFd = socket(AF_INET, SOCK_STREAM, 0) ;
+    
+    if (serverFd < 0) {
         perror("socket");
         exit(EXIT_FAILURE);
     } 
     
     else {
-        printf("Socket created successfully with file descriptor: %d\n", fd) ;
+        printf("Socket created successfully with file descriptor: %d\n", serverFd) ;
     }
 
     struct sockaddr_in serverAddress = {} ;
@@ -25,11 +131,12 @@ int main() {
     serverAddress.sin_port = htons(1234) ; 
     serverAddress.sin_addr.s_addr = INADDR_ANY ; 
 
-    int bindResult = bind(fd, (const struct sockaddr*) &serverAddress, sizeof(serverAddress)) ;
+    int bindResult = bind(serverFd, (const struct sockaddr*) &serverAddress, sizeof(serverAddress)) ;
     
     if (bindResult < 0) {
         perror("bind") ;
-        close(fd) ;
+
+        close(serverFd) ;
         exit(EXIT_FAILURE) ;
     } 
     
@@ -37,47 +144,36 @@ int main() {
         printf("Socket bound successfully to port 1234\n") ;    
     }
 
-    int listenResult = listen(fd, 5) ;
+    int listenResult = listen(serverFd, 2) ;
     
     if (listenResult < 0) {
         perror("listen") ;
-        close(fd) ;  
+        close(serverFd) ;  
         exit(EXIT_FAILURE) ;
     }
 
     while (true) {
         struct sockaddr_in clientAddress = {} ;
         socklen_t clientAddressLength = sizeof(clientAddress) ;
-        int clientFd = accept(fd, (struct sockaddr*) &clientAddress, &clientAddressLength) ;
+        int clientFd = accept(serverFd, (struct sockaddr*) &clientAddress, &clientAddressLength) ;
         
         if (clientFd < 0) {
-            perror("accept") ;
-            close(fd) ;
-            exit(EXIT_FAILURE) ;
+            continue ;
         } 
 
         else {
             printf("Accepted connection from client with file descriptor: %d\n", clientFd) ;
         }
+
+        while (true) {
+            int err = singleClientComm(clientFd) ;
+
+            if (err) {
+                printf("Some problem with connection, exiting.\n") ;
+                break ;
+            }
+        }
         
-        char readBuffer[1024] = {} ;
-        ssize_t bytesRead = read(clientFd, readBuffer, sizeof(readBuffer) - 1) ;
-        if (bytesRead < 0) {
-            perror("read") ;
-            close(clientFd) ;
-            continue;
-        }
-
-        printf("Received message: %.*s\n", (int)bytesRead, readBuffer) ;
-
-        char writeBuffer[1024] = "Hello from server!" ;
-        ssize_t bytesWritten = write(clientFd, writeBuffer, strlen(writeBuffer)) ;
-        if (bytesWritten < 0) {
-            perror("write") ;
-            close(clientFd) ;       
-            continue;
-        }
-
-         close(clientFd) ;
+        close(clientFd) ;
     }
 }
